fe035e1eaff64c95dd0a2b45b2efa185bb66bf84
Artem Tumin <arttumin@gmail.com>
2014-11-05 21:25:06 +0300

    animations

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,3 +1,29 @@
+$.fn.split = function(){
+	var element = $(this);
+
+	var defaultText = $.trim( $(this).text() );
+	var htmlString = defaultText.split('');
+	var splitResult = '';
+	
+	for(var i = 0; i < htmlString.length; i++) {
+		if(htmlString[i] == ' ') {
+			splitResult += '<div style="display: inline-block; position:relative;">' + '&nbsp;' + '</div>';
+		}
+		splitResult += '<div style="display: inline-block; position:relative;">' + htmlString[i] + '</div>';
+	}
+
+	element.html(splitResult);
+
+	return {
+		reverse: function(){
+			element.html(defaultText);
+		}
+	}
+};
+
+var h1Anim = $('.header-main-desc').split();
+var h2Anim = $('.header-sec-desc').split();
+
 var App = (function(){
 	'use strict';
 
@@ -16,13 +42,24 @@ var App = (function(){
 		tl.staggerFromTo($('.portfolio-li'), 1, {x: '200%', opacity: '0'}, {x: '0%', opacity: '1', ease:Circ.easeOut}, 0.2);
 	//Processes
 	var tl1 = new TimelineMax({paused: true});
-		tl1.staggerFromTo($('.proc-li'), 1, {x: '200%', opacity: '0'}, {x: '0%', opacity: '1', ease:Circ.easeOut}, 0.2);
+		tl1.from($('.proc-ol'), 1, {x: '200%', opacity: '0'}, {x: '0%', opacity: '1', ease:Circ.easeOut}, 0.2);
 	//Footer
 	var tl2 = new TimelineMax({paused: true});
-		tl2.from($('.main-footer address'), 0.6, {x: '200%', opacity: '0', ease:Circ.easeOut, delay: 0.4})
-		   .staggerFrom($('main-footer address, .contacts-link'), 0.4, {x: '200%', opacity: '0', ease:Circ.easeOut}, 0.2)
-		   .from($('.copy'), 0.6, {x: '200%', opacity: '0', ease:Circ.easeOut, delay: 0.4});
+		tl2.from($('.main-footer .section-wrap'), 0.6, {x: '200%', opacity: '0', ease:Circ.easeOut, delay: 0.4});
+	//Header animation
+	var tl3 = new TimelineMax({paused: true});
+		tl3.staggerFrom($('.header-main-desc > div'), .85, {opacity:0, scale:0, y:100, rotationX:80, transformOrigin:"0% 200% -50",  ease:Back.easeOut }, 0.02);
 
+	TweenLite.set(".header-main-desc", {perspective:400});
+
+	var tl4 = new TimelineMax({paused: true});
+		tl4.staggerFrom($('.header-sec-desc > div'), .85, {opacity:0, scale:0, y:100, rotationX:80, transformOrigin:"0% 200% -50",  ease:Back.easeOut, delay: 2 }, 0.02);
+
+	TweenLite.set(".header-main-desc", {perspective:400});
+
+	tl3.restart();
+	tl4.restart();
+	
 	specBlock.add(portBlock).add(aboutBlock).add(procBlock).add(footerBlock).appear();
 
 	specBlock.on('appear', function(event, $all_appeared_elements) {
