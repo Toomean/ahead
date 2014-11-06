110271fcddfc73896bcb7682a3ffdc76c8e15641
Artem Tumin <arttumin@gmail.com>
2014-11-06 12:07:02 +0300

    fixes

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,29 +1,3 @@
-$.fn.split = function(){
-	var element = $(this);
-
-	var defaultText = $.trim( $(this).text() );
-	var htmlString = defaultText.split('');
-	var splitResult = '';
-	
-	for(var i = 0; i < htmlString.length; i++) {
-		if(htmlString[i] == ' ') {
-			splitResult += '<div style="display: inline-block; position:relative;">' + '&nbsp;' + '</div>';
-		}
-		splitResult += '<div style="display: inline-block; position:relative;">' + htmlString[i] + '</div>';
-	}
-
-	element.html(splitResult);
-
-	return {
-		reverse: function(){
-			element.html(defaultText);
-		}
-	}
-};
-
-var h1Anim = $('.header-main-desc').split();
-var h2Anim = $('.header-sec-desc').split();
-
 var App = (function(){
 	'use strict';
 
@@ -48,19 +22,15 @@ var App = (function(){
 		tl2.from($('.main-footer .section-wrap'), 0.6, {x: '200%', opacity: '0', ease:Circ.easeOut, delay: 0.4});
 	//Header animation
 	var tl3 = new TimelineMax({paused: true});
-		tl3.staggerFrom($('.header-main-desc > div'), .85, {opacity:0, scale:0, y:100, rotationX:80, transformOrigin:"0% 200% -50",  ease:Back.easeOut }, 0.02);
-
-	TweenLite.set(".header-main-desc", {perspective:400});
+		tl3.from($('.header-main-desc'), 0.6, {x: '200%', opacity: '0', ease:Circ.easeOut, delay: 0.4});
 
 	var tl4 = new TimelineMax({paused: true});
-		tl4.staggerFrom($('.header-sec-desc > div'), .85, {opacity:0, scale:0, y:100, rotationX:80, transformOrigin:"0% 200% -50",  ease:Back.easeOut, delay: 2 }, 0.02);
-
-	TweenLite.set(".header-main-desc", {perspective:400});
+		tl4.from($('.header-sec-desc'), 0.6, {x: '200%', opacity: '0', ease:Circ.easeOut, delay: 0.8});
+	
+	specBlock.add(portBlock).add(aboutBlock).add(procBlock).add(footerBlock).appear();
 
 	tl3.restart();
 	tl4.restart();
-	
-	specBlock.add(portBlock).add(aboutBlock).add(procBlock).add(footerBlock).appear();
 
 	specBlock.on('appear', function(event, $all_appeared_elements) {
 
