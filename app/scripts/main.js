5018ac127458a50caf13c802fc8a3a9b4d74b251
Artem Tumin <arttumin@gmail.com>
2014-12-12 20:25:39 +0300

    work in progress

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -53,15 +53,63 @@ var App = (function(){
 	(function(){
 		var $formStart = $('.js-show-form');
 		var $overlay = $('.overlay');
+		var $overlayClose = $('.overlay-close');
+
+		var $form = $('.feedback-form form');
+		var $formBtn = $form.find('.js-form-submit');
+		var $fieldset = $form.find('fieldset');
+		var $label = $form.find('label');
+		var $textarea = $form.find('textarea');
+		var $inputElem = $form.find('input, textarea');
 
 		var tl1 = new TimelineMax({paused: true});
 		tl1.to( $overlay, 0.6, { bottom: '0', ease:Circ.easeOut })
-		   .to( $('body'), 0, { overflow: 'hidden' } );
+		   .to( $('body'), 0, { overflow: 'hidden' } )
+		   .staggerFromTo($fieldset.add($formBtn), 1, {y: '-200%', opacity: '0'}, {y: '0%', opacity: '1', ease:Circ.easeOut}, 0.2);
+
+		$(function(){
+			$inputElem.each( function(){
+				var $label = $(this).prev();
+
+				if( $(this).val() != '' ) {
+					$label.addClass('active');
+				}
+			});
+		});
+
+		//Form events
+		$textarea.autosize();
 
 		$formStart.click( function(){
-			alert('go');
+			tl1.timeScale(1);
 			tl1.restart();
+		});
+
+		$overlayClose.click( function(){
+			tl1.timeScale(2);
+			tl1.reverse();
+		});
+
+		$label.click( function(){
+			var $nearInput = $(this).next();
+			if($nearInput.val() == '') {
+				$(this).addClass('active');
+				$nearInput.focus();
+			}
+		});
+
+		$inputElem.focus( function(){
+			var $label = $(this).prev();
+			$label.addClass('active');
 		})
+
+		$inputElem.focusout( function(){
+			var $label = $(this).prev();
+
+			if( $(this).val() == '' ) {
+				$label.removeClass('active');
+			}
+		});
 
 	})();
 })();
